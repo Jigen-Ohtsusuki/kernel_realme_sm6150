f4034b11aae331204d4028811c194ebe3fc902d2
Jigen-Ohtsusuki <abhishekpenkey@gmail.com>
2026-08-31 06:21:13 +0000

    arm64: avoid "sp" as a clang global register var
    Some clang versions don't reliably support binding "sp" as a
    persistent global register variable. Read it via inline asm on
    each use instead.
    
    Change-Id: I08d82b37a9eb826d103605c0b85d534cfa5e469b
    Signed-off-by: Jigen-Ohtsusuki <abhishekpenkey@gmail.com>

--- arch/arm64/include/asm/stack_pointer.h
+++ arch/arm64/include/asm/stack_pointer.h
@@ -1,10 +1,18 @@
-/* SPDX-License-Identifier: GPL-2.0 */
 #ifndef __ASM_STACK_POINTER_H
 #define __ASM_STACK_POINTER_H
 
 /*
- * how to get the current stack pointer from C
+ * how to get the current stack pointer in C
+ *
+ * Some versions of Clang do not reliably support "sp" as a global
+ * register variable on AArch64, so read it explicitly via inline asm
+ * on every use instead of binding a persistent register variable.
  */
-register unsigned long current_stack_pointer asm ("sp");
+#define current_stack_pointer					\
+({								\
+	unsigned long __csp;					\
+	asm ("mov %0, sp" : "=r" (__csp));			\
+	__csp;							\
+})
 
 #endif /* __ASM_STACK_POINTER_H */
